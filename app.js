3a184453e094bee4f43e5d74acf86c4e75d05ca8
cki515 <kwangilstory@gmail.com>
2023-05-25 14:22:32 +1200

    add mysql2 and db connection and load query

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 import express from "express";
+import mysql from "mysql2/promise";
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  port: "3307",
+  password: "",
+  database: "famous_saying",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+app.get("/famous_saying", async (req, res) => {
+  const [rows] = await pool.query(
+    "SELECT * FROM famous_saying ORDER BY id DESC"
+  );
+  res.json(rows);
+});
+
+app.get("/famous_saying/:id", async (req, res) => {
+  const { id } = req.params;
+  const [row] = await pool.query("SELECT * FROM famous_saying WHERE id=?", [
+    id,
+  ]);
+  if (row.length == 0) {
+    res.status(404).send("Page is not found");
+    return;
+  }
+  res.json(row);
 });
 
 app.get("/about", (req, res) => {
